90ca03796d2a021cab355f12171b0a3e8a171cf0
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-03-24 00:09:18 +0200

    Update Notebook.cpp

--- Notebook.cpp
+++ Notebook.cpp
@@ -1,26 +1,74 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 #include "Notebook.hpp"
+#include "Direction.hpp"
 using namespace std;
 using namespace ariel;
+using ariel::Direction;
 
 
 namespace ariel{
     Notebook::Notebook(){
-        
+        unordered_map<int, unordered_map<int, char[100]> > pages;
     }
     
     void Notebook::write(int page, int row, int col, ariel::Direction dir, string msg){
-
+        if(page < 0 || row < 0 || col < 0 || col > 99 || (int)msg.length() > 100){
+            throw invalid_argument("invalid input!");
+        }
+        if(dir == Direction::Horizontal){
+            if((int)msg.length() > 100 - col){
+                throw invalid_argument("invalid input!");
+            }
+        }
     }
     string Notebook::read(int page, int row, int col, ariel::Direction dir, int len){
-        return " ";
+        if(page < 0 || row < 0 || col < 0 || col > 99 || len < 0 || len > 100){
+            throw invalid_argument("invalid input!");
+        }
+        string text = "";
+        if(dir == Direction::Horizontal){
+            if(len > 100 - col){
+                throw invalid_argument("invalid input!");
+            }
+            for (int i = col; i < len; i++)
+            {
+                text += pages.at(page).at(row)[i];
+            }
+        }
+        if(dir == Direction::Vertical){
+            for (int j = 0; j < len; j++)
+            {
+                text += pages.at(page).at(row+j)[col];
+            }
+        }
+        return text;
     }
-    void Notebook::erase(int page, int row, int col, ariel::Direction dir, int len){
 
+    void Notebook::erase(int page, int row, int col, ariel::Direction dir, int len){
+        if(page < 0 || row < 0 || col < 0 || col > 99 || len < 0 || len > 100){
+            throw invalid_argument("invalid input!");
+        }
+        if(dir == Direction::Horizontal){
+            if(len > 100 - col){
+                throw invalid_argument("invalid input!");
+             }
+            for (int i = col; i < len; i++)
+            {
+                pages.at(page).at(row)[i] = '~';
+            }    
+        }
+        if(dir == Direction::Vertical){
+            for (int j = 0; j < len; j++)
+            {
+                pages.at(page).at(row+j)[col] = "~"
+            }
+        }
     }
+
     void Notebook::show(int page){
 
     }
 
 }
-
